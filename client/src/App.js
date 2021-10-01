1acc0f3772c63f9fa5051ff8e3958a371914d513
Hieracosphynx <cortezmiguel05@gmail.com>
2021-10-01 14:24:39 -0700

    Temperature gauge

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import Raspberry from "./components/raspberry/Raspberry";
-import "./App.css";
+import Raspberry from './components/raspberry/Raspberry';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Raspberry />
+    <div className='App'>
+      <div className='container-column-temp'>
+        <Raspberry />
+      </div>
     </div>
   );
 }
--- client/src/components/raspberry/Raspberry.js
+++ client/src/components/raspberry/Raspberry.js
@@ -1,7 +1,30 @@
-import Card from "@mui/material/Card";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import RaspberryStatus from './RaspberryStatus';
 
 const Raspberry = () => {
-  return <Card variant="outline">Raspberry</Card>;
+  return (
+    <Card
+      variant='outlined'
+      sx={{
+        bgcolor: 'background.paper',
+        boxShadow: 1,
+        borderRadius: 1,
+        p: 2,
+        maxWidth: 300,
+        backgroundColor: 'gray',
+        opacity: '90%',
+      }}
+    >
+      <CardContent>
+        <Typography align='center' variant='h5' sx={{ color: 'black' }}>
+          Raspberry
+        </Typography>
+        <RaspberryStatus />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Raspberry;
