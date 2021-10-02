8d65da7d680e3ae0586c9ec866f04fe231a46cf5
Hieracosphynx <cortezmiguel05@gmail.com>
2021-10-01 20:36:09 -0700

    Raspberry component

---
+++ client/src/components/raspberry/Raspberry.js
@@ -0,0 +1,14 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import RaspberryStatus from './RaspberryStatus';
+
+const Raspberry = () => {
+  return (
+    <CardContent>
+      <RaspberryStatus />
+    </CardContent>
+  );
+};
+
+export default Raspberry;
